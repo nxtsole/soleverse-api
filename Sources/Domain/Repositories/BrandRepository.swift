26ac8b8f16e41a3624d1514e6451471f6b9e64a5
Emanuel Guerrero <emanuelguerrerodev@gmail.com>
2024-05-21 22:52:08 -0400

    add repository interfaces

---
+++ Sources/Domain/Repositories/BrandRepository.swift
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SoleVerse API open source project
+//
+// Copyright (c) 2023 NXTSOLE and the SoleVerse API project authors
+// Licensed under BSD 3-Clause "New" or "Revised" License
+//
+// See LICENSE for license information
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+public protocol BrandRepository: Repository {
+    func read() async throws -> [BrandEntity]
+    func read(id: BrandType) async throws -> BrandEntity
+}
---
+++ Sources/Domain/Repositories/DesignerRepository.swift
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SoleVerse API open source project
+//
+// Copyright (c) 2023 NXTSOLE and the SoleVerse API project authors
+// Licensed under BSD 3-Clause "New" or "Revised" License
+//
+// See LICENSE for license information
+//
+// SPDX-License-Identifier: BSD-3-Clause
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+public protocol DesignerRepository: Repository {
+    func read(id: Int) async throws -> DesignerEntity
+}
